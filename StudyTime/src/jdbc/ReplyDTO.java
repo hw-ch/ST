b168e7de7e1881c8777153e3e60810e1005792ec
pjione <a41924250@gmail.com>
2023-02-16 03:16:57 +0900

    asd

--- StudyTime/src/jdbc/ReplyDTO.java
+++ StudyTime/src/jdbc/ReplyDTO.java
@@ -2,10 +2,13 @@ package jdbc;
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import lombok.AllArgsConstructor;
 import lombok.Data;
 @AllArgsConstructor
 @Data
+=======
+>>>>>>> parent of 2bf326a (20230216 setting)
 =======
 >>>>>>> parent of 2bf326a (20230216 setting)
 =======
