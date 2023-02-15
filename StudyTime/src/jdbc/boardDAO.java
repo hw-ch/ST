5c7f3daf8cc91eacd14bad778a39ca3d3d1551d4
김도영 <difbfl4750@gmail.com>
2023-02-15 16:18:09 +0900

    230215

--- StudyTime/src/jdbc/boardDAO.java
+++
@@ -1,6 +0,0 @@
-package jdbc;
-
-public class boardDAO {
-	
-	public class boardinsert()
-}
---
+++ StudyTime/src/jdbc/studyJoinDTO.java
@@ -0,0 +1,13 @@
+package jdbc;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class studyJoinDTO {
+	
+	private String sjNo, userId, sNo, approve, regDate;
+
+
+}
