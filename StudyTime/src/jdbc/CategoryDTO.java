926e26c8e51c9862f64aeac4ae8e8bd009820000
hw-ch <wone8115@uos.ac.kr>
2023-02-15 15:13:20 +0900

    230215

---
+++ StudyTime/src/jdbc/CategoryDTO.java
@@ -0,0 +1,15 @@
+package jdbc;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class CategoryDTO {
+	
+	private String cNo, category1, category2;
+
+	
+
+	
+}
---
+++ StudyTime/src/jdbc/UserDTO.java
@@ -0,0 +1,12 @@
+package jdbc;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class UserDTO {
+	
+	private String userId, password, nickName, name, ts, gender, image, phone;
+	
+}
