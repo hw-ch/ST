a88cbb0144b2a8aaecc9b8442d2a9cb02cafcbb9
pjione <a41924250@gmail.com>
2023-02-15 21:46:44 +0900

    Update BoardDTO.java

--- StudyTime/src/jdbc/BoardDTO.java
+++ StudyTime/src/jdbc/BoardDTO.java
@@ -6,6 +6,10 @@ import lombok.Data;
 @AllArgsConstructor
 @Data
 public class BoardDTO {
+<<<<<<< HEAD
+=======
+	
+>>>>>>> parent of e749161 (Update BoardDTO.java)
 	private String bNo, subject, content, nickName, userId, hit, good, bad, regDate, uptDate, replyNum;
 
 }
