2e2554f553b74c34abbb5fb71861a7f167abe88f
pjione <a41924250@gmail.com>
2023-02-16 15:38:23 +0900

    sadf

--- StudyTime/src/jdbc/StudyDAO.java
+++ StudyTime/src/jdbc/StudyDAO.java
@@ -13,93 +13,15 @@ import util.ConnectionPool;
 
 public class StudyDAO {
 
+	
+	private static Connection conn;
 	private static PreparedStatement pstmt;
 	private static String sql;
 	private static ResultSet rs;
-	StudyDTO sdto;
-	private static Connection conn;
-	private static int result;
 	public StudyDAO() {
 		try {conn = ConnectionPool.get();} catch (NamingException | SQLException e) {e.printStackTrace();}
 	}
 	
-//	public static ArrayList<StudyDTO> getAllList() throws SQLException, NamingException{
-//			
-//		ArrayList<StudyDTO> studys = new ArrayList<StudyDTO>();
-//		
-//		try {
-//			sql = " SELECT * FROM study ORDER BY sNo DESC ";
-//	
-//			conn = ConnectionPool.get();
-//			
-//			pstmt = conn.prepareStatement(sql);
-//	
-//			rs = pstmt.executeQuery();
-//			
-//			
-//			
-//			while(rs.next()) {		
-//				studys.add(new StudyDTO(rs.getString(1),
-//									rs.getString(2),
-//									rs.getString(3),
-//									rs.getString(4),
-//									rs.getString(5),
-//									rs.getString(6),
-//									rs.getString(7),
-//									rs.getString(8),
-//									rs.getString(9),
-//									rs.getString(10),
-//									rs.getString(11),
-//									rs.getString(12),
-//									rs.getString(13),
-//									rs.getString(14),
-//									rs.getString(15)));
-//			}
-//	
-//				return studys;
-//			
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		} finally {
-//			if(pstmt != null)
-//			pstmt.close();
-//			if(conn != null)
-//			conn.close();
-//		}
-//	
-//		return studys;
-//	
-//		}
-//	
-	
-	public static boolean apply(String sNo) throws NamingException, SQLException {
-		
-		try {
-			
-			sql = "UPDATE study SET apply=? "
-					+ " WHERE sNo=? ";
-
-			pstmt = conn.prepareStatement(sql);
-
-			pstmt.setString(1, "승인");
-			pstmt.setString(2, sNo);
-
-			int result = pstmt.executeUpdate();
-			if (result == 1) {
-				return true;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			if(pstmt != null)
-			pstmt.close();
-			if(conn != null)
-			conn.close();
-		}
-
-		return false;
-
-	}
 	
 	
 	//스터디목록(지원)
@@ -202,7 +124,7 @@ public class StudyDAO {
 				pstmt.setString(11, scontent);
 				pstmt.setString(12, process);
 				pstmt.setString(13, sNo);
-			
+				
 				if(pstmt.executeUpdate()==1) return true;
 				
 			}catch (Exception e) {
@@ -214,7 +136,7 @@ public class StudyDAO {
 			
 			return false;
 		}
-	// 스터디 삭제(지원)
+	// 스터디 삭제
 		public static boolean studyDelete(String sNo) {
 			sql = "DELETE FROM study WHERE sNo = ?";
 			try {
@@ -230,5 +152,27 @@ public class StudyDAO {
 			}
 			return false;
 		}
+		
+	public static boolean apply(String sNo) throws NamingException, SQLException {
+		
+		try {
+			sql = "UPDATE study SET apply=? WHERE sNo=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, "승인");
+			pstmt.setString(2, sNo);
+			int result = pstmt.executeUpdate();
+			if (result == 1) {
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) try { pstmt.close(); } catch(Exception e) {e.printStackTrace();}
+			if (conn != null) try { conn.close(); } catch(Exception e) {e.printStackTrace();}
+			if (rs != null) try { rs.close(); } catch (SQLException e) {e.printStackTrace();}
+		}
+		return false;
+
+	}
 
 }
