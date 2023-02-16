840c1b1e055773222a01102261b179034cfdb074
pjione <a41924250@gmail.com>
2023-02-16 10:35:12 +0900

    0216 10:35 merge

--- StudyTime/src/jdbc/StudyDAO.java
+++ StudyTime/src/jdbc/StudyDAO.java
@@ -8,9 +8,6 @@ import java.util.ArrayList;
 
 import javax.naming.NamingException;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-
 import util.ConnectionPool;
 
 public class StudyDAO {
