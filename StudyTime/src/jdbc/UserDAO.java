823bf20829bddca9b7823d333367ee78759f35b2
Doo <secure3141@gmail.com>
2023-02-17 14:36:37 +0900

    230217 14:36 Doohyun merge

--- StudyTime/src/jdbc/UserDAO.java
+++ StudyTime/src/jdbc/UserDAO.java
@@ -65,7 +65,7 @@ public class UserDAO {
 			} catch (NamingException | SQLException e) {
 				e.printStackTrace();
 			}
-		}finally {
+		finally {
 			  if (pstmt != null) try { pstmt.close(); } 
 			  	catch(Exception e) {e.printStackTrace();}
 			  if (conn != null) try { conn.close(); } 
@@ -89,6 +89,50 @@ public class UserDAO {
 			pstmt.setString(1, sWriter);
 
 			rs = pstmt.executeQuery();
+
+			JSONArray study = new JSONArray();
+			
+			while(rs.next()) {
+				JSONObject obj = new JSONObject();
+				obj.put("sTitle", rs.getString(1));
+				obj.put("sWriter", rs.getString(2));
+				obj.put("joinCnt", rs.getString(3));
+				obj.put("startDate", rs.getString(4));
+				obj.put("process", rs.getString(5));
+				obj.put("expDate", rs.getString(6));
+				obj.put("cNo", rs.getString(7));
+			
+				study.add(obj);
+			}
+				
+			return study.toJSONString();
+			
+		}finally {
+			  if (pstmt != null) try { pstmt.close(); } 
+			  	catch(Exception e) {e.printStackTrace();}
+			  if (conn != null) try { conn.close(); } 
+	            catch(Exception e) {e.printStackTrace();}
+		}
+		
+	}
+	
+	
+	
+	//회원가입 (완료)
+	public static boolean join(String userId, String password, String name, String nickname, String gender, String phone, String image) throws NamingException, SQLException{
+		sql = "INSERT INTO user(userid, password, name, nickname, "
+				+ " gender, phone, image) VALUES(?, ?, ?, ?, ?, ?, ?)";
+		
+		try {
+			con = ConnectionPool.get();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, userId);
+			pstmt.setString(2, password);
+			pstmt.setString(3, name);
+			pstmt.setString(4, nickname);
+			pstmt.setString(5, gender);
+			pstmt.setString(6, phone);
+			pstmt.setString(7, image);
 			
 			JSONArray study = new JSONArray();
 			
@@ -218,6 +262,11 @@ public class UserDAO {
 	public static boolean dropout(String userId){
 	
 	try {
+	//로그인 (완료)
+	public static int login(String id, String userPass) throws NamingException, SQLException {
+		
+		sql = "SELECT userId, password FROM user WHERE userId=? ";
+		
 		
 		sql = "DELETE from user where userid=? ";
 
@@ -267,6 +316,32 @@ public class UserDAO {
 			pstmt = conn.prepareStatement(sql);
 	
 			pstmt.setString(1, userId);
+			con = ConnectionPool.get(); //커넥션 풀 사용
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			
+			if(!rs.next()) return 1; //아이디가 존재하지 않는 경우
+			if(!userPass.equals(rs.getString("password"))) return 2;	//아이디는 존재하지만 비밀번호가 다른 경우
+			return 0; //아이디와 비밀번호 일치 !! 로그인 성공
+			
+		}	finally {
+			if(rs != null) rs.close();
+			if(pstmt!= null) pstmt.close();
+			if(con != null) con.close();
+		}
+		
+	}
+	
+	//카카오 로그인(완료)
+	public static int kakaoLogin(String id) throws NamingException, SQLException {
+		
+		sql = "SELECT userId FROM user WHERE userId=?";
+		
+		try {
+			con = ConnectionPool.get();	
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
 			
 			rs = pstmt.executeQuery();
 			
@@ -293,6 +368,10 @@ public class UserDAO {
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
+		}	finally {
+			if(rs != null) rs.close();
+			if(pstmt!= null) pstmt.close();
+			if(con != null) con.close();
 		}
 	
 		return user;
@@ -322,5 +401,79 @@ public class UserDAO {
 		            catch(Exception e) {e.printStackTrace();}
 				}
 			}
+	//아이디 찾기(완료)
+	public static String idFind(String userName, String userPhone) throws NamingException, SQLException {
+		
+		sql = "SELECT userid FROM user WHERE name=? and phone=?";
+		
+		String id = null; // DB에 있는 아이디 찾기
+		
+		try {
+			con = ConnectionPool.get();	
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, userName);
+			pstmt.setString(2, userPhone);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				id = rs.getString("userId");	//아이디 반환 !
+			}
+			
+		}	finally {
+			if(rs != null) rs.close();
+			if(pstmt!= null) pstmt.close();
+			if(con != null) con.close();
+		}
+		return id;
+	}
+	
+	//비밀번호 찾기(완료)
+	public static String pwFind(String userid, String name, String userPhone) throws NamingException, SQLException {
+		
+		sql = "SELECT password FROM user WHERE userId=? and name=? and phone=? ";
+		String password = null;
+		
+		try {
+			con = ConnectionPool.get();	
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, userid);
+			pstmt.setString(2, name);
+			pstmt.setString(3, userPhone);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				password = rs.getString("password");
+			}
+		}	finally {
+			if(rs != null) rs.close();
+			if(pstmt!= null) pstmt.close();
+			if(con != null) con.close();
+		}
+		return password;
+	}
+	
+	//비밀번호 찾기를 누른 후 임시비밀번호로 변경되는 메서드
+	public static boolean updatePw(String id, String pw) throws NamingException, SQLException {
+		
+		sql = "UPDATE user set password=? WHERE userId=?";
+		
+	try {
+		con = ConnectionPool.get();	
+		pstmt = con.prepareStatement(sql);
+		pstmt.setString(1, id);
+		pstmt.setString(2, pw);
+		
+		int result = pstmt.executeUpdate();
+		if(result == 1) {
+			return true;
+		}
+	}	finally {
+		if(pstmt!= null) pstmt.close();
+		if(con != null) con.close();
+	}
+	return false;
+}
 	
 }
