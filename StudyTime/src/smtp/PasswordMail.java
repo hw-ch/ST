f70597438c129347d9de0eeaf5a3238ba70b6805
Doo <secure3141@gmail.com>
2023-02-17 11:58:27 +0900

    230217 임시 비밀번호 보내기

---
+++ StudyTime/src/smtp/PasswordMail.java
@@ -0,0 +1,42 @@
+package smtp;
+
+import java.util.*;
+
+import jdbc.UserDAO;
+
+public class PasswordMail {
+	
+	public static void sending(String id, String name, String phone) {
+		
+		//인증코드 생성 숫자 , 랜덤 대문자 섞기
+		Random ran = new Random();
+		StringBuffer br = new StringBuffer();
+		for(int i =0; i<6; i++) {
+			if(ran.nextBoolean()) {
+				br.append((int)(ran.nextInt(10))); // 0~9까지 랜덤 한 숫자
+			}	else {
+				br.append((char)((int)(Math.random()*26)+65));	// 아스키 코드 65 ~ 90은 대문자 
+			}
+		}
+		
+		
+		Map<String, String> emailInfo = new HashMap<String, String>();
+
+		emailInfo.put("from", "secure3141@gmail.com"); //보내는 사람 , 변동 불가, 반드시 본인의 메일주소
+		emailInfo.put("to", id); //받는 사람
+		emailInfo.put("subject", "STUDY TIME 계정 - 비밀번호 변경 안내");// 메일 제목
+		emailInfo.put("content", name + " 회원님 비밀번호 변경을 도와드리겠습니다.\n"
+									  + "비밀번호 변경 인증번호는 [" + br + "]" + "입니다.");// 메일 내용
+
+		emailInfo.put("format", "text/plain;charset=UTF-8");// 메일의 형식을 설정 일단 text 로 고정
+
+
+		try {
+			GoogleSMTP smtpServer = new GoogleSMTP(); //메일 전송 객체 생성
+			smtpServer.emailSending(emailInfo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
