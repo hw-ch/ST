351f7f31216b8f2b31a287aebc5544df0bd8a8b7
Doo <secure3141@gmail.com>
2023-02-17 15:56:36 +0900

    230217

--- StudyTime/src/jdbc/UserDAO.java
+++ StudyTime/src/jdbc/UserDAO.java
@@ -11,17 +11,17 @@ public class UserDAO {
 	private static PreparedStatement pstmt;
 	private static String sql;
 	private static ResultSet rs;
-	private static Connection con;
+	private static Connection conn;
 	
 	
-	//회원가입 (완료)
-	public static boolean join(String userId, String password, String name, String nickname, String gender, String phone, String image) throws NamingException, SQLException{
+	//회원가입 (두현)
+	public static boolean join(String userId, String password, String name, String nickname, String gender, String phone, String image){
 		sql = "INSERT INTO user(userid, password, name, nickname, "
 				+ " gender, phone, image) VALUES(?, ?, ?, ?, ?, ?, ?)";
 		
 		try {
-			con = ConnectionPool.get();
-			pstmt = con.prepareStatement(sql);
+			conn = ConnectionPool.get();
+			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, userId);
 			pstmt.setString(2, password);
 			pstmt.setString(3, name);
@@ -34,22 +34,27 @@ public class UserDAO {
 			if(result == 1) {
 				return true;
 			}
-		} finally {
-			if(pstmt!= null) pstmt.close();
-			if(con != null) con.close();
+		}	catch (Exception e) {
+			e.printStackTrace();
+		}  finally {
+			try {
+				if(pstmt!= null) pstmt.close();
+				if(conn!=null) conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 		return false;
 	}
 	
-	//로그인 (완료)
-	public static int login(String id, String userPass) throws NamingException, SQLException {
+	//로그인 (두현)
+	public static int login(String id, String userPass) {
 		
 		sql = "SELECT userId, password FROM user WHERE userId=? ";
 		
-		
 		try {
-			con = ConnectionPool.get(); //커넥션 풀 사용
-			pstmt = con.prepareStatement(sql);
+			conn = ConnectionPool.get(); //커넥션 풀 사용
+			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, id);
 			rs = pstmt.executeQuery();
 			
@@ -57,22 +62,29 @@ public class UserDAO {
 			if(!userPass.equals(rs.getString("password"))) return 2;	//아이디는 존재하지만 비밀번호가 다른 경우
 			return 0; //아이디와 비밀번호 일치 !! 로그인 성공
 			
-		}	finally {
-			if(rs != null) rs.close();
-			if(pstmt!= null) pstmt.close();
-			if(con != null) con.close();
+		}	catch (Exception e) {
+			e.printStackTrace();
+		} 
+		finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt!= null) pstmt.close();
+				if(conn != null) conn.close();
+			}catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
-		
+		return -1;
 	}
 	
-	//카카오 로그인(완료)
-	public static int kakaoLogin(String id) throws NamingException, SQLException {
+	//카카오 로그인(두현)
+	public static int kakaoLogin(String id) {
 		
 		sql = "SELECT userId FROM user WHERE userId=?";
 		
 		try {
-			con = ConnectionPool.get();	
-			pstmt = con.prepareStatement(sql);
+			conn = ConnectionPool.get();	
+			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, id);
 			
 			rs = pstmt.executeQuery();
@@ -81,24 +93,31 @@ public class UserDAO {
 			
 			return 0;	// DB에 존재하는 경우
 			
-		}	finally {
-			if(rs != null) rs.close();
-			if(pstmt!= null) pstmt.close();
-			if(con != null) con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 	
+		finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt!= null) pstmt.close();
+				if(conn != null) conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
-		
+		return -1;
 	}
 	
-	//아이디 찾기(완료)
-	public static String idFind(String userName, String userPhone) throws NamingException, SQLException {
+	//아이디 찾기(두현)
+	public static String idFind(String userName, String userPhone) {
 		
 		sql = "SELECT userid FROM user WHERE name=? and phone=?";
 		
 		String id = null; // DB에 있는 아이디 찾기
 		
 		try {
-			con = ConnectionPool.get();	
-			pstmt = con.prepareStatement(sql);
+			conn = ConnectionPool.get();	
+			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, userName);
 			pstmt.setString(2, userPhone);
 			
@@ -108,23 +127,30 @@ public class UserDAO {
 				id = rs.getString("userId");	//아이디 반환 !
 			}
 			
-		}	finally {
-			if(rs != null) rs.close();
-			if(pstmt!= null) pstmt.close();
-			if(con != null) con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt!= null) pstmt.close();
+				if(conn != null) conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 		return id;
 	}
 	
-	//비밀번호 찾기(완료)
-	public static String pwFind(String userid, String name, String userPhone) throws NamingException, SQLException {
+	//비밀번호 찾기(두현)
+	public static String pwFind(String userid, String name, String userPhone) {
 		
 		sql = "SELECT password FROM user WHERE userId=? and name=? and phone=? ";
 		String password = null;
 		
 		try {
-			con = ConnectionPool.get();	
-			pstmt = con.prepareStatement(sql);
+			conn = ConnectionPool.get();	
+			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, userid);
 			pstmt.setString(2, name);
 			pstmt.setString(3, userPhone);
@@ -134,22 +160,29 @@ public class UserDAO {
 			while(rs.next()) {
 				password = rs.getString("password");
 			}
-		}	finally {
-			if(rs != null) rs.close();
-			if(pstmt!= null) pstmt.close();
-			if(con != null) con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+		finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt!= null) pstmt.close();
+				if(conn != null) conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 		return password;
 	}
 	
-	//비밀번호 찾기를 누른 후 임시비밀번호로 변경되는 메서드
-	public static boolean updatePw(String id, String pw) throws NamingException, SQLException {
+	//비밀번호 찾기를 누른 후 임시비밀번호로 변경되는 메서드(두현)
+	public static boolean updatePw(String id, String pw) {
 		
 		sql = "UPDATE user set password=? WHERE userId=?";
 		
 	try {
-		con = ConnectionPool.get();	
-		pstmt = con.prepareStatement(sql);
+		conn = ConnectionPool.get();	
+		pstmt = conn.prepareStatement(sql);
 		pstmt.setString(1, id);
 		pstmt.setString(2, pw);
 		
@@ -157,11 +190,19 @@ public class UserDAO {
 		if(result == 1) {
 			return true;
 		}
-	}	finally {
-		if(pstmt!= null) pstmt.close();
-		if(con != null) con.close();
+	} catch (Exception e) {
+		e.printStackTrace();
+	}	
+	finally {
+		try {
+			if(pstmt!= null) pstmt.close();
+			if(conn != null) conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	return false;
 }
+	
 	
 }
