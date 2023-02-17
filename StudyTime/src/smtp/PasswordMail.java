a1ce95c55465d00c3bafbc77480d39a62cf6767c
Doo <secure3141@gmail.com>
2023-02-17 17:11:41 +0900

    230217 임시비밀번호 완료

--- StudyTime/src/smtp/PasswordMail.java
+++ StudyTime/src/smtp/PasswordMail.java
@@ -28,7 +28,8 @@ public class PasswordMail {
 		emailInfo.put("to", id); //받는 사람
 		emailInfo.put("subject", "STUDY TIME 계정 - 비밀번호 변경 안내");// 메일 제목
 		emailInfo.put("content", name + " 회원님 비밀번호 변경을 도와드리겠습니다.\n"
-									  + "비밀번호 변경 인증번호는 [" + br + "]" + "입니다.");// 메일 내용
+									  + "임시 비밀번호는 [" + br + "]" + "입니다.\n"
+									  + "임시 비밀번호로 로그인하세요!");// 메일 내용
 
 		emailInfo.put("format", "text/plain;charset=UTF-8");// 메일의 형식을 설정 일단 text 로 고정
 
